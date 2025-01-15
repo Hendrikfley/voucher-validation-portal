25a5324f37b8f3434164988b8cd3afadcb1ecaad
Hendrikfley <Hendrik.fley@web.de>
2025-01-15 16:41:05 +0100

    minor styling fixes

--- src/frontend.php
+++ src/frontend.php
@@ -6,28 +6,26 @@ function render()
 {
     ob_start();
     ?>
-
-    <>
-        <!-- <h1>QR Code Scanner</h1> -->
-        <div id="reader"></div>
-        <div id="loadingSpinner">Überprüfe Code ...</div>
-        <details>
-            <summary id="manualCodeToggle">Code manuell eingeben</summary>
-            <form>
-                <label id="label" for="code">Gib den/die Codes hier ein:</label>
-                <input type="text" id="code" name="Code" />
-                <button id="validateButton">Überprüfen</button>
-            </form>
-        </details>
-        </div>
-        <div id="popup">
-            <h2 id="result"></h2>
-            <p id="popupCode"></p>
-            <p id="popupMessage"></p>
-            <button id="abortButton">Abbrechen</button>
-            <button id="redeemButton">Einlösen</button>
-        </div>
-        </body>
-        <?php
-        return ob_get_clean();
+    <!-- <h1>QR Code Scanner</h1> -->
+    <div id="reader"></div>
+    <div id="loadingSpinner">Überprüfe Code ...</div>
+    <details>
+        <summary id="manualCodeToggle">Code manuell eingeben</summary>
+        <form>
+            <label id="label" for="code">Gib den/die Codes hier ein:</label>
+            <input type="text" id="code" name="Code" />
+            <button id="validateButton">Überprüfen</button>
+        </form>
+    </details>
+    </div>
+    <div id="popup">
+        <h2 id="result"></h2>
+        <p id="popupCode"></p>
+        <p id="popupMessage"></p>
+        <button id="abortButton">Abbrechen</button>
+        <button id="redeemButton">Einlösen</button>
+    </div>
+    </body>
+    <?php
+    return ob_get_clean();
 }
